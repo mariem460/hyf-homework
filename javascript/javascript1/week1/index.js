78ef30ed5f086e787e683068d81f66b33e01b89a
mariem460 <mariem.oueslati.hajjem@gmail.com>
2021-10-28 14:51:50 +0200

    modify some code based on mentor's feedback

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -7,14 +7,12 @@ console.log (`You will be ${age} years old in 2027`);
 /*dog age calculator*/
 let dogYearOfBirth = 2020;
 let dogYearFuture = 2027;
-let dogYear = dogYearFuture - dogYearOfBirth ;
+let dogYear = (dogYearFuture - dogYearOfBirth)*7 ;
 let shouldShowResultInDogYears = true;
 if (shouldShowResultInDogYears){
     console.log(`Your dog will be ${dogYear} dog years old in 2027`);
 }
-else{
-    console.log(`our dog will be 10 human years old in 2027`)
-}
+
 
 /*house pricey*/
 let wide1 = 8;
@@ -23,14 +21,28 @@ let deep1 = 10;
 let gardenSize1 = 100;
 let volumeInMeters1 = wide1 * height1 * gardenSize1;
 let housePrice1 = volumeInMeters1 * 2.5 * 1000 + gardenSize1 *300;
-console.log(`Peter's house will costs ${housePrice1}`);
+console.log(housePrice1);
+if(housePrice1 > 2.500){
+    console.log(`Peter's house will costs ${housePrice1} and that is not good price`);
+}
+else if (housePrice1 < 2.500){
+    console.log(`Peter's house will costs ${housePrice1}, so it could be good price`);
+}
+
 let wide2 = 5;
 let height2 = 8;
 let deep2 = 11;
 let gardenSize2 = 70;
 let volumeInMeters2 = wide2 * height2 * gardenSize2;
 let housePrice2 = volumeInMeters2 * 2.5 * 1000 + gardenSize2 *300;
-console.log(`Jilia's house will costs ${housePrice2}`);
+if(housePrice2 > 1.000){
+    console.log(`Jilia's house will costs ${housePrice2}, so it is not a good price`);
+}
+else if (housePrice2 < 1.000){
+    console.log(`Julia's house will costs ${housePrice1}, so it could be good price`);
+}
+
+
 
 
 /*start up name*/
@@ -40,7 +52,9 @@ var foodItems = ["Bannana", "Apple", "Orange"];
 var randomFirstWords = firstWords[Math.floor(Math.random() * firstWords.length)];
 var randomSecondWords = secondWords[Math.floor(Math.random() * secondWords.length)];
 var startupName = randomFirstWords +" "+ randomSecondWords;
+var numOfCharc = startupName.length;
 console.log(startupName);
+console.log(`the startup name will have ${numOfCharc} charcters`)
 
 
 
