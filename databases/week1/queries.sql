45d5241e652b5871f31aed3b4880f52a9075915d
mariem460 <mariem.oueslati.hajjem@gmail.com>
2022-02-07 10:11:49 +0100

    make some changes

--- databases/week1/queries.sql
+++ databases/week1/queries.sql
@@ -11,9 +11,11 @@ from task
 WHERE due_date IS NULL;
 
 --3 Find all the tasks that are marked as done;
-SELECT count (status_id)
+SELECT  COUNT(task.title)
 from task
-WHERE status_id= 3;
+JOIN status
+ON task.status_id = status.name
+WHERE status.name = "done";
 
 --4 find all the tasks that are not marked as done
 SELECT count (status_id)
@@ -22,21 +24,22 @@ WHERE status_id != 3;
 
 --5 Get all the tasks, sorted with the most recently created first
 
-SELECT title
+SELECT title, created
 from task
-ORDER BY created;
+ORDER BY created DESC;
 
 --6 Get the single most recently created task
 SELECT title
 FROM task
-ORDER BY created
+ORDER BY created DESC
 LIMIT 1;
 
 --7 Get the title and due date of all tasks where the title or description contains database
-SELECT title, due_date
+SELECT title, description
 from task
-WHERE due_date IS NOT NULL
-AND title IS NOT NULL;
+WHERE task.title LIKE "%database%" 
+OR task.description LIKE "%database%";
+
 
 --8 Get the title and status (as text) of all tasks
 SELECT task.title, status.name
@@ -49,14 +52,15 @@ SELECT status.name, COUNT(task.status_id) AS "n_time(s)"
 FROM status
 JOIN task
 ON status.id = task.status_id
-GROUP BY status.name;
+GROUP BY task.status_id;
 
 --10 Get the names of all statuses, sorted by the status with most tasks first
-SELECT status.name, task.created
+SELECT status.name, COUNT(task.status_id) AS "n times"
 FROM status
 JOIN task
 ON status.id = task.status_id
-ORDER BY task.created ASC;
+GROUP BY task.status_id
+ORDER BY "n_times" ASC;
 
 
 
